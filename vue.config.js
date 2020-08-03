428b0fc8d41a4b5c1a323636a5223985512f1fa1
RexHung <zehung860486@gmail.com>
2020-08-04 02:06:12 +0800

    update: add deploy.sh

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
 module.exports = {
+  // publicPath: '/Hexschool-firebase-bmi-practice/dist/'
+  // 改為只推 dist 資料夾內容上 github 所以 publicPath 改為根目錄
   publicPath: '/Hexschool-firebase-bmi-practice/dist/'
 }
